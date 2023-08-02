e7f6b6e1048126c6115387ee1800af2172c5bfec
creonaveen <naveenkumar@creoit.com>
2023-08-02 18:01:03 +0530

    temp

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,76 +2,101 @@ import { Injectable } from '@angular/core';
 import { Observable, first } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
-  login_details:any=[{id:"1234",phone:"9445886295",pass:"123456"},
-  {id:"12345",phone:"9788586295",pass:"123456"},
-  {id:"123",phone:"8695489921",pass:"123456"}]
-  studentrec:any=[{id:123,firstname:"parth",lastname:"sokku",age:"22",sub:"",class:"",phone:"9807867812"},
-  {id:124,firstname:"parth",lastname:"sokku",age:"22",sub:"",class:"",phone:"9807867812"},
-  {id:125,firstname:"parth",lastname:"sokku",age:"22",sub:"",class:"",phone:"9807867812"}]
-  student_details:any=this.studentrec
-  teacher_details:any=[];
-  inLogin(){
+  login_details: any = [
+    { id: '1234', phone: '9445886295', pass: '123456' },
+    { id: '12345', phone: '9788586295', pass: '123456' },
+    { id: '123', phone: '8695489921', pass: '123456' },
+  ];
+  studentrec: any = [
+    {
+      id: 123,
+      firstname: 'parth',
+      lastname: 'sokku',
+      age: '22',
+      sub: '',
+      class: '',
+      phone: '9807867812',
+    },
+    {
+      id: 124,
+      firstname: 'parth',
+      lastname: 'sokku',
+      age: '22',
+      sub: '',
+      class: '',
+      phone: '9807867812',
+    },
+    {
+      id: 125,
+      firstname: 'parth',
+      lastname: 'sokku',
+      age: '22',
+      sub: '',
+      class: '',
+      phone: '9807867812',
+    },
+  ];
+ 
+  inLogin() {
     return !!localStorage.getItem('token');
   }
-  validCheck(login:any){
-    console.log(this.login_details)
-    const obser=new Observable((val)=>{
-    const elemen=this.login_details.find((val:any)=>(val.phone==login.phone && val.pass==val.pass));
-    if(elemen?.phone==login.phone && elemen?.pass==login.pass){
-      val.next(elemen.id);
-      val.complete();
-    }
-    else{
-      val.error({error:'no phone number'});
-      val.complete();
-    }
+  validCheck(login: any) {
+    console.log(this.login_details);
+    const obser = new Observable((val) => {
+      const elemen = this.login_details.find(
+        (val: any) => val.phone == login.phone && val.pass == val.pass
+      );
+      if (elemen?.phone == login.phone && elemen?.pass == login.pass) {
+        val.next(elemen.id);
+        val.complete();
+      } else {
+        val.error({ error: 'no phone number' });
+        val.complete();
+      }
     });
     return obser;
   }
-  studentDetailsSaved(studentrecord:any){ 
-    const obser=new Observable((val)=>{
-      if(!("id" in studentrecord)){
-        studentrecord["id"]=(Math.floor(Math.random()*10000));
+  studentDetailsSaved(studentrecord: any) {
+    const obser = new Observable((val) => {
+      if (!('id' in studentrecord)) {
+        studentrecord['id'] = Math.floor(Math.random() * 10000);
       }
-      this.student_details.push(studentrecord);
-      val.next(this.student_details);
+      this.studentrec.push(studentrecord);
+      val.next(this.studentrec);
       val.complete();
-    })
+    });
     return obser;
   }
-  teacherDetailsSaved(teacherrecord:any){
-    const obser=new Observable((val)=>{
-      this.teacher_details.push(teacherrecord);
-      val.next(this.teacher_details);
-      val.complete()
-    })
+  
+  studentEditSave(editvalue: any) {
+    console.log(this.studentrec);
+    const obser = new Observable((val) => {
+      const elemen = this.studentrec.find( (val: any) => val.id == editvalue.id );
+      if(editvalue){
+        elemen['firstname'] = editvalue.firstname;
+        elemen['lastname'] = editvalue.lastname;
+        elemen['age'] = editvalue.age;
+        elemen['sub'] = editvalue.sub;
+        elemen['class'] = editvalue.class;
+        elemen['phone'] = editvalue.phone;
+        val.next(elemen);
+        val.complete();
+      }else{
+
+      }
+       
+       
+    });
     return obser;
   }
-  studentEditSave(editvalue:any){
-    console.log(this.student_details)
-    const obser=new Observable((val)=>{
-      const elemen=this.student_details.find((val:any)=>(val.id==editvalue.id));
-        if(elemen?.id==editvalue.id){     
-          elemen["firstname"]=editvalue.firstname
-          elemen["lastname"]=editvalue.lastname
-          elemen["age"]=editvalue.age
-          elemen["sub"]=editvalue.sub
-          elemen["class"]=editvalue.class
-          elemen["phone"]=editvalue.phone
-          val.next(this.student_details);
-          val.complete();
-        }
-     })
-    return obser
-  }
-  tableList(value:any){
-    const obser=new Observable((val)=>{
-      val.next(value);
-      val.complete()
-    })
-  return obser
+  tableList() {
+    const obser = new Observable((val) => {
+      val.next(this.studentrec);
+      val.complete();
+    });
+    return obser;
   }
 }
--- src/app/studentdetails/studentdetails.component.ts
+++ src/app/studentdetails/studentdetails.component.ts
@@ -8,58 +8,53 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-studentdetails',
   templateUrl: './studentdetails.component.html',
-  styleUrls: ['./studentdetails.component.css']
+  styleUrls: ['./studentdetails.component.css'],
 })
 export class StudentdetailsComponent {
-  constructor(public heroservive:HeroService, public matDialog:MatDialog ){
-    this.heroservive.student_details;
+  constructor(public heroservive: HeroService, public matDialog: MatDialog) {
+    this.tableList();
   }
-  student=new FormGroup ({ firstname:new FormControl('',[Validators.required]),
-  lastname:new FormControl(''),
-  age:new FormControl(''),
-  sub:new FormControl(''),
-  class:new FormControl(''),
-  phone:new FormControl(''),
+  student = new FormGroup({
+    firstname: new FormControl('', [Validators.required]),
+    lastname: new FormControl(''),
+    age: new FormControl(''),
+    sub: new FormControl(''),
+    class: new FormControl(''),
+    phone: new FormControl(''),
   });
-  update=new FormGroup ({ firstname:new FormControl('',[Validators.required]),
-  lastname:new FormControl('',[Validators.required]),
-  age:new FormControl('',[Validators.required]),
-  sub:new FormControl('',[Validators.required]),
-  class:new FormControl('',[Validators.required]),
-  phone:new FormControl('',[Validators.required]),
-  id:new FormControl('')
-  });
-  displayedColumns: string[] = ["id","firstname","lastname","age","sub","class","phone",'action'];
-  dataSource:any;
-  studentSaved(){
-    alert("save the value");
-   if(this.student.valid){
-    this.heroservive.studentDetailsSaved(this.student.value).subscribe(value=>{
-      this.tableList(value);
+  displayedColumns: string[] = [
+    'id',
+    'firstname',
+    'lastname',
+    'age',
+    'sub',
+    'class',
+    'phone',
+    'action',
+  ];
+  dataSource: any;
+  studentSaved() {
+    if (this.student.valid) {
+      this.heroservive
+        .studentDetailsSaved(this.student.value)
+        .subscribe((value) => {
+          this.tableList();
+        });
+      return;
+    }
+  }
+  open(content: any, row: any) {
+    this.matDialog.open(content, { height: '95%', width: '50%' });
+  }
+  onUpdate() {
+    // alert('save the value');
+    // this.heroservive.studentEditSave(this.update.value).subscribe((value) => {
+    //   this.tableList();
+    // });
+  }
+  tableList() {
+    this.heroservive.tableList().subscribe((value) => {
+      this.dataSource = value;
     });
-    return;
-   }
-  }
-  open(content:any,row:any){
-    this.matDialog.open(content,{height: '95%',
-    width: '50%'});
-    this.update.controls['firstname'].setValue(row.firstname);
-    this.update.controls['lastname'].setValue(row.lastname);
-    this.update.controls['sub'].setValue(row.sub);
-    this.update.controls['age'].setValue(row.age);
-    this.update.controls['class'].setValue(row.class);
-    this.update.controls['phone'].setValue(row.phone);
-    this.update.controls['id'].setValue(row.id);
-  }
-  onUpdate(){
-    alert("save the value")
-    this.heroservive.studentEditSave(this.update.value).subscribe((value)=>{
-      this.tableList(value);
-    })
-  }
-  tableList(value:any){
-  this.heroservive.tableList(value).subscribe((value)=>{
-    this.dataSource=value;
-  })
   }
 }
